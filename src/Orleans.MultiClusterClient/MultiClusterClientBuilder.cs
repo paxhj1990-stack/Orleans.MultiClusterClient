33fb3bef5a0e2221dfd156b77c7271fca0c2c6e1
DESKTOP-48QVM9E\DELL <935714690@qq.com>
2026-06-04 05:39:08 +0800

    support FromKeyedServices

--- src/Orleans.MultiClusterClient/MultiClusterClientBuilder.cs
+++ src/Orleans.MultiClusterClient/MultiClusterClientBuilder.cs
@@ -11,6 +11,7 @@ internal sealed class MultiClusterClientBuilder(IServiceCollection services, ICo
 
     public void AddClient(String name, Action<IClientBuilder> configureDelegate)
     {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
         var clientBuilder = new RemoteClusterClientBuilder(name, configuration);
         configureDelegate(clientBuilder);
         _clientBuilders.Add(name, clientBuilder);
@@ -20,6 +21,11 @@ internal sealed class MultiClusterClientBuilder(IServiceCollection services, ICo
     {
         var provider = new MultiClusterProvider(_clientBuilders);
         services.TryAddSingleton<IMultiClusterClient>(provider);
+        services.TryAddSingleton<IMultiClusterProvider>(provider);
+        foreach (var name in _clientBuilders.Keys)
+        {
+            services.TryAddKeyedSingleton(name, (sp, key) => sp.GetRequiredService<IMultiClusterProvider>()[(String)key!]);
+        }
         _ = services.AddHostedService(sp => provider);
     }
 }
